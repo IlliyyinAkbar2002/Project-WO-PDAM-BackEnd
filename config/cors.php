359e797b82c50fd6e47771cc65d2cff96772f40f
Illiyyin Akbar A <illiyyin.akbar123@gmail.com>
2026-04-10 14:39:15 +0700

    chore: Update dependencies in composer.lock and modify CORS settings to allow all origins

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
