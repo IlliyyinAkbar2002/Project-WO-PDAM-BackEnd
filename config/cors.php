bfc1fdd9be353ed97f12aa95bc5400e0a70b460c
Illiyyin Akbar A <illiyyin.akbar123@gmail.com>
2026-04-16 23:27:32 +0700

    chore: Update .gitignore to include 'env.md' and '.cursor'; modify CORS settings to restrict allowed origins for improved security

--- config/cors.php
+++ config/cors.php
@@ -15,8 +15,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins_patterns' => ['/^https:\/\/.*\.ngrok-free\.dev$/'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
